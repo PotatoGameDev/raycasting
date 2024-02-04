318987838bc9dee074bf60918fe53bec328ed211
halfdan <piotr.kaczmarski@pixelheartsoftware.com>
2024-02-04 23:05:33 +0100

    Added player and arrow controls

--- main.cpp
+++ main.cpp
@@ -13,7 +13,6 @@ auto mfbDeleter = [](mfb_window* ptr) {
 int image_scale = 20;
 int color_scale = 100;
 
-
 std::vector<std::vector<int>> worldMap
 {
   {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
@@ -42,15 +41,20 @@ std::vector<std::vector<int>> worldMap
   {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 };
 
-int main() {
-    const unsigned width = 800;
-    const unsigned height = 600;
+const unsigned width = 800;
+const unsigned height = 600;
 
-    std::pair<float, float> player_pos{ 2.0, 2.0 };
+const float speed = 0.1;
 
-    std::vector<uint32_t> buffer(width * height);
-    std::vector<std::vector<uint32_t>> buffer_internal(width, std::vector<uint32_t>(height, 0x11111100));
+std::pair<float, float> player_pos{ 2.0, 2.0 };
 
+std::vector<uint32_t> buffer(width * height);
+std::vector<std::vector<uint32_t>> buffer_internal(width, std::vector<uint32_t>(height, 0x11111100));
+
+
+
+
+void draw_map(const std::vector<std::vector<uint32_t>>& buffer_internal, std::vector<uint32_t>& buffer, std::pair<float, float> player_pos) {
     for (unsigned x = 0; x < height; ++x) {
         for (unsigned y = 0; y < width; ++y) {
             buffer[x * width + y] = buffer_internal[y][x];
@@ -58,20 +62,49 @@ int main() {
             if (x/image_scale < worldMap.size() && y/ image_scale < worldMap[0].size()) {
                 buffer[x * width + y] = worldMap[x/image_scale][y/image_scale] * color_scale;
             }
+	    if (x/image_scale == static_cast<int>(player_pos.first) && y/image_scale == static_cast<int>(player_pos.second)) {
+		buffer[x * width + y] = 0xffffffff;
+	    }
         }
     }
+}
 
-    std::unique_ptr<mfb_window, decltype(mfbDeleter)> window(mfb_open("Frodo Lives", width, height), mfbDeleter);
+
+void keyboard(struct mfb_window *window, mfb_key key, mfb_key_mod mod, bool isPressed) {
+    // Remember to close the window in some way
+    if(key == KB_KEY_ESCAPE) {
+        mfb_close(window);
+    } else if (key == KB_KEY_UP) {
+	player_pos.first -= speed;	
+    } else if (key == KB_KEY_DOWN) {
+	player_pos.first += speed;	
+    } else if (key == KB_KEY_LEFT) {
+	player_pos.second -= speed;	
+    } else if (key == KB_KEY_RIGHT) {
+	player_pos.second += speed;	
+    }
+
+
+
+}
+
+
+int main() {
+    std::unique_ptr<struct mfb_window, decltype(mfbDeleter)> window(mfb_open("Frodo Lives", width, height), mfbDeleter);
     if (!window) {
         return -1;
     }
 
+    mfb_set_keyboard_callback(window.get(), keyboard);
+
     while (mfb_wait_sync(window.get())) {
         if (mfb_update(window.get(), buffer.data()) < 0) {
-            // If mfb_update returns a negative value, the window should close
             break;
         }
+	draw_map(buffer_internal, buffer, player_pos);
     }
 
     return 0;
 }
+
+
